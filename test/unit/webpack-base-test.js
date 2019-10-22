f3d98bdd64ddfbb3cf01acf87097f65f83986b23
Nicholas-Qiu <qiudingfei@gmail.com>
2019-10-22 10:08:50 +0800

    ### 修改内容 * 修改配置

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('webpack.base.js test case', () => {
 
     console.log(baseConfig);
     it('entry', () => {
-        assert.equal(baseConfig.entry.index,'F:/Web/my-project/builder-webpack/test/smoke/template/src/page/index/index.js');
-        assert.equal(baseConfig.entry.search,'F:/Web/my-project/builder-webpack/test/smoke/template/src/page/search/index.js');
+        assert.equal(baseConfig.entry.index,'F:/Web/builder-webpack-qdf/test/smoke/template/src/page/index/index.js');
+        assert.equal(baseConfig.entry.search,'F:/Web/builder-webpack-qdf/test/smoke/template/src/page/search/index.js');
     });
 });
